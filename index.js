ebfc029e5752b850aceeb0e111106de69c1cff72
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-22 17:00:52 +0600

    make api for service and single service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -16,12 +16,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("CarRepair").collection("service");
-  console.log("genius car db connected");
-  // perform actions on the collection object
-  client.close();
-});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("CarRepair").collection("service");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+  } finally {
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running car repair server");
