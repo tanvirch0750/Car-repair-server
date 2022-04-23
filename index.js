5e1cee1aaf63ee2297f0a55b879a5aa72e2cae33
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-23 18:15:44 +0600

    implemented dlete functionality and route

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ const run = async () => {
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
     });
+
+    // delete service
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 };
