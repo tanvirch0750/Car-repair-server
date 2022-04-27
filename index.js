bc4deb74a93b274bf6415790a0cc53f5fd683e94
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-27 17:45:31 +0600

    add a middleware

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.apqen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
