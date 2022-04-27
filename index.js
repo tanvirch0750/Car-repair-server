b5fd816043fc9e5903c99a198e3aeed21ce8b1fe
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-27 22:34:24 +0600

    back to old code

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const { createProxyMiddleware } = require("http-proxy-middleware");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -11,26 +9,6 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-
-const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({
-      message: "Unauthorized access",
-    });
-  }
-
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "forbidden access" });
-    } else {
-      console.log("decoded", decoded);
-      req.decoded = decoded;
-      next();
-    }
-  });
-};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.apqen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -109,7 +87,7 @@ const run = async () => {
       res.send(result);
     });
 
-    app.get("/order", verifyJWT, async (req, res) => {
+    app.get("/order", async (req, res) => {
       const email = req.query.email;
       const query = { email };
       const cursor = orderCollection.find(query);
