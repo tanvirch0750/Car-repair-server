6af1f96baf7a8b802c048ba9cc6d54b2cca4bd0f
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-27 17:50:57 +0600

    remove middleware

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.apqen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
