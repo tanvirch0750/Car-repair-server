9f277e7beb2f028e144e6a89c54ca5abbe2ef759
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-28 14:29:05 +0600

    testing

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
